c16cfbc102880fd4a77b049117e8706e23af809f
Norbert Huffschmid <124078844+NHuffschmid@users.noreply.github.com>
2025-09-13 19:16:14 +0200

    migrated PlaylistSelector code into PlaylistPanel

--- www/client/src/components/playlist/PlaylistPanel.tsx
+++ www/client/src/components/playlist/PlaylistPanel.tsx
@@ -1,19 +1,28 @@
+
 import React from 'react';
 import { useTranslation } from "react-i18next";
-import PlaylistSelector from './PlaylistSelector';
 import { usePlaylistContext } from './PlaylistContext';
 
 const PlaylistPanel: React.FC = () => {
     const { t } = useTranslation();
-    const { playlists, setPlaylists } = usePlaylistContext();
+    const { playlists, setPlaylists, selected, setSelected } = usePlaylistContext();
 
     const handleAdd = () => {
-        setPlaylists([...playlists, 'New Playlist']);
+        const newName = `New Playlist ${playlists.length + 1}`;
+        setPlaylists([...playlists, newName]);
+        setSelected(newName);
     };
 
     const handleDelete = () => {
         if (playlists.length > 0) {
-            setPlaylists(playlists.slice(0, -1));
+            const idx = playlists.indexOf(selected);
+            const newPlaylists = playlists.filter(pl => pl !== selected);
+            setPlaylists(newPlaylists);
+            if (newPlaylists.length > 0) {
+                setSelected(newPlaylists[Math.max(0, idx - (idx === newPlaylists.length ? 1 : 0))]);
+            } else {
+                setSelected('');
+            }
         }
     };
 
@@ -26,7 +35,18 @@ const PlaylistPanel: React.FC = () => {
             backgroundColor: 'gray',
             padding: '0.2rem'
         }}>
-            <PlaylistSelector playlists={playlists} />
+            <div>
+                <select
+                    id="playlist-combobox"
+                    style={{ fontSize: '1.2rem' }}
+                    value={selected}
+                    onChange={e => setSelected(e.target.value)}
+                >
+                    {playlists.map((pl, idx) => (
+                        <option key={idx} value={pl}>{pl}</option>
+                    ))}
+                </select>
+            </div>
             <button
                 title={t('Create new playlist') ?? ''}
                 onClick={handleAdd} style={{ marginLeft: '1rem' }}>
