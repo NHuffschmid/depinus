5fe590ead33ca8f792ca9d159a8f26c618746431
Norbert Huffschmid <124078844+NHuffschmid@users.noreply.github.com>
2025-09-14 19:04:24 +0200

    enhanced swagger.yaml for playlist and added backend code and tests (2 still failing)

---
+++ www/server/api/controllers/playlist.js
@@ -0,0 +1,127 @@
+const logger = require("./helpers/logger")(__filename);
+const db = require('./helpers/db');
+
+module.exports = {
+    getPlaylists,
+    postPlaylist,
+    getPlaylist,
+    deletePlaylist,
+    getPlaylistCompositions,
+    addCompositionToPlaylist,
+    updatePlaylistOrder,
+    removeCompositionFromPlaylist
+};
+
+function getPlaylists(req, res) {
+    db.all('SELECT id, name FROM playlist ORDER BY name;', [], (err, rows) => {
+        if (err) {
+            res.status(500).json({ 'message': err.toString() });
+            logger.error(err.message);
+        } else {
+            res.json(rows);
+        }
+    });
+}
+
+function postPlaylist(req, res) {
+    const name = req.swagger.params.name.value;
+    db.run('INSERT INTO playlist (name) VALUES (?);', [name], function (err, lastID) {
+        if (err) {
+            res.status(500).json({ 'message': err.toString() });
+            logger.error(err.message);
+        } else {
+            res.status(200).json({ id: lastID, name: name });
+        }
+    });
+}
+
+function getPlaylist(req, res) {
+    const id = req.swagger.params.id.value;
+    db.get('SELECT id, name FROM playlist WHERE id = ?;', [id], (err, row) => {
+        if (err) {
+            res.status(500).json({ 'message': err.toString() });
+            logger.error(err.message);
+        } else if (!row) {
+            res.status(404).json({ 'message': 'Playlist not found' });
+        } else {
+            res.json(row);
+        }
+    });
+}
+
+function deletePlaylist(req, res) {
+    const id = req.swagger.params.id.value;
+    db.run('DELETE FROM playlist WHERE id = ?;', [id], (err) => {
+        if (err) {
+            res.status(500).json({ 'message': err.toString() });
+            logger.error(err.message);
+        } else {
+            res.status(204).send();
+        }
+    });
+}
+
+function getPlaylistCompositions(req, res) {
+    const id = req.swagger.params.id.value;
+    db.all(`SELECT c.id, c.name, c.duration, c.composer_id
+          FROM playlist_composition pc
+          JOIN composition c ON pc.composition_id = c.id
+          WHERE pc.playlist_id = ?
+          ORDER BY pc.position;`, [id], (err, rows) => {
+        if (err) {
+            res.status(500).json({ 'message': err.toString() });
+            logger.error(err.message);
+        } else {
+            res.json(rows);
+        }
+    });
+}
+
+function addCompositionToPlaylist(req, res) {
+    const playlistId = req.swagger.params.id.value;
+    const compositionId = req.swagger.params.compositionId.value;
+    const position = req.swagger.params.position.value || 0;
+    db.run('INSERT INTO playlist_composition (playlist_id, composition_id, position) VALUES (?, ?, ?);',
+        [playlistId, compositionId, position], (err) => {
+            if (err) {
+                res.status(500).json({ 'message': err.toString() });
+                logger.error(err.message);
+            } else {
+                res.status(204).send();
+            }
+        });
+}
+
+function updatePlaylistOrder(req, res) {
+    const playlistId = req.swagger.params.id.value;
+    const order = req.body;
+    const updateTasks = order.map(item => {
+        return new Promise((resolve, reject) => {
+            db.run('UPDATE playlist_composition SET position = ? WHERE playlist_id = ? AND composition_id = ?;',
+                [item.position, playlistId, item.compositionId], (err) => {
+                    if (err) reject(err);
+                    else resolve();
+                });
+        });
+    });
+    Promise.all(updateTasks)
+        .then(() => res.status(204).send())
+        .catch(err => {
+            res.status(500).json({ 'message': err.toString() });
+            logger.error(err.message);
+        });
+}
+
+function removeCompositionFromPlaylist(req, res) {
+    const playlistId = req.swagger.params.id.value;
+    const compositionId = req.swagger.params.compositionId.value;
+    db.run('DELETE FROM playlist_composition WHERE playlist_id = ? AND composition_id = ?;',
+        [playlistId, compositionId], (err) => {
+            if (err) {
+                res.status(500).json({ 'message': err.toString() });
+                logger.error(err.message);
+            } else {
+                res.status(204).send();
+            }
+        });
+}
---
+++ www/server/test/api/controllers/playlist.js
@@ -0,0 +1,131 @@
+const should = require('should');
+const request = require('supertest');
+
+const BACKEND_URL = 'http://localhost:5000';
+
+describe('controllers', function () {
+  describe('playlist', function () {
+    this.slow(1000);
+    let playlistId;
+    let compositionId = 1; // Set to a valid composition ID in your DB
+
+    describe('GET /playlist', function () {
+      it('should return all playlists', function (done) {
+        request(BACKEND_URL)
+          .get('/playlist')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function (err, res) {
+            should.not.exist(err);
+            res.body.should.be.an.Array();
+            done();
+          });
+      });
+    });
+
+    describe('POST /playlist', function () {
+      it('should create a new playlist', function (done) {
+        request(BACKEND_URL)
+          .post('/playlist')
+          .set('Content-Type', 'application/x-www-form-urlencoded')
+          .set('Accept', 'application/json')
+          .send('name=Test Playlist')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function (err, res) {
+            should.not.exist(err);
+            res.body.name.should.eql('Test Playlist');
+            playlistId = res.body.id;
+            playlistId.should.be.above(0);
+            done();
+          });
+      });
+    });
+
+    describe('GET /playlist/{id}', function () {
+      it('should return the created playlist', function (done) {
+        request(BACKEND_URL)
+          .get('/playlist/' + playlistId)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function (err, res) {
+            should.not.exist(err);
+            res.body.id.should.eql(playlistId);
+            res.body.name.should.eql('Test Playlist');
+            done();
+          });
+      });
+    });
+
+    describe('POST /playlist/{id}/compositions', function () {
+      it('should add a composition to the playlist', function (done) {
+        request(BACKEND_URL)
+          .post('/playlist/' + playlistId + '/compositions')
+          .field('compositionId', compositionId)
+          .field('position', 1)
+          .expect(204)
+          .end(function (err, res) {
+            should.not.exist(err);
+            done();
+          });
+      });
+    });
+
+    describe('GET /playlist/{id}/compositions', function () {
+      it('should return compositions in the playlist', function (done) {
+        request(BACKEND_URL)
+          .get('/playlist/' + playlistId + '/compositions')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function (err, res) {
+            should.not.exist(err);
+            res.body.should.be.an.Array();
+            done();
+          });
+      });
+    });
+
+    describe('PATCH /playlist/{id}/compositions', function () {
+      it('should update the order of compositions in the playlist', function (done) {
+        const order = [{ compositionId: compositionId, position: 2 }];
+        request(BACKEND_URL)
+          .patch('/playlist/' + playlistId + '/compositions')
+          .send(order)
+          .set('Accept', 'application/json')
+          .expect(204)
+          .end(function (err, res) {
+            should.not.exist(err);
+            done();
+          });
+      });
+    });
+
+    describe('DELETE /playlist/{id}/compositions', function () {
+      it('should remove a composition from the playlist', function (done) {
+        request(BACKEND_URL)
+          .delete('/playlist/' + playlistId + '/compositions')
+          .field('compositionId', compositionId)
+          .expect(204)
+          .end(function (err, res) {
+            should.not.exist(err);
+            done();
+          });
+      });
+    });
+
+    describe('DELETE /playlist/{id}', function () {
+      it('should delete the playlist', function (done) {
+        request(BACKEND_URL)
+          .delete('/playlist/' + playlistId)
+          .expect(204)
+          .end(function (err, res) {
+            should.not.exist(err);
+            done();
+          });
+      });
+    });
+  });
+});
