61c684c57e6219ecb90064a5415e9d8ff510c8cb
Norbert Huffschmid <124078844+NHuffschmid@users.noreply.github.com>
2025-11-22 23:28:27 +0100

    reworked build script

--- scripts/build_release_package.js
+++ scripts/build_release_package.js
@@ -3,9 +3,6 @@
 const { execSync } = require('child_process');
 const path = require('path');
 
-const scriptDir = __dirname;
-const projectDir = path.resolve(scriptDir, '..');
-
 const consoleYellowColor = '\x1b[33m%s\x1b[0m';
 const consoleGreenColor = '\x1b[32m%s\x1b[0m';
 const consoleRedColor = '\x1b[31m%s\x1b[0m';
@@ -13,15 +10,19 @@ const consoleRedColor = '\x1b[31m%s\x1b[0m';
 const isWindows = process.platform === 'win32';
 
 try {
-  console.log(consoleYellowColor, `Running clean script...`);
-  execSync('npm run clean', { stdio: 'inherit', cwd: projectDir });
+  console.log(consoleYellowColor, 'Cleaning workspace...');
+  execSync('npm run clean', { stdio: 'inherit' });
 
+  /*
   console.log(consoleYellowColor, 'Activating virtual Python environment and running build...');
   if (isWindows) {
-    execSync('.\\venv\\Scripts\\activate && npm run build', { stdio: 'inherit', cwd: projectDir, shell: true });
+    execSync('.\\venv\\Scripts\\activate && npm run build', { stdio: 'inherit', shell: true });
   } else {
-    execSync('source ./venv/bin/activate && npm run build', { stdio: 'inherit', cwd: projectDir, shell: '/bin/bash' });
+    execSync('source ./venv/bin/activate && npm run build', { stdio: 'inherit', shell: '/bin/bash' });
   }
+  */
+ 
+  console.log(consoleGreenColor, 'Release package successfully built.');
 } catch (error) {
   console.log(consoleRedColor, error.message || error);
 }
