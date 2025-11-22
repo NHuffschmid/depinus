54f3d274e5e5d103df70bd99b04d0839a432bd85
Norbert Huffschmid <124078844+NHuffschmid@users.noreply.github.com>
2025-11-22 16:33:52 +0100

    removed function call

--- scripts/build_release_package.js
+++ scripts/build_release_package.js
@@ -6,11 +6,7 @@ const path = require('path');
 const scriptDir = __dirname;
 const projectDir = path.resolve(scriptDir, '..');
 
-function runBuildSteps() {
-  console.log(`Running clean in ${projectDir} ...`);
-  execSync('npm run clean', { stdio: 'inherit', cwd: projectDir });
+console.log(`Running clean in ${projectDir} ...`);
+execSync('npm run clean', { stdio: 'inherit', cwd: projectDir });
 
-  // more to come
-}
-
-runBuildSteps();
+// more to come
