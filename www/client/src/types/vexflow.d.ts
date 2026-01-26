cfa6bed8194788c9b110d1aadb874913b87b1602
Norbert Huffschmid <124078844+NHuffschmid@users.noreply.github.com>
2026-01-26 16:29:12 +0100

    added some dummy demo notes to get familiar with VexFlow

--- www/client/src/types/vexflow.d.ts
+++ www/client/src/types/vexflow.d.ts
@@ -13,6 +13,7 @@ declare module 'vexflow' {
         constructor(x: number, y: number, width: number);
         addClef(clef: string): this;
         addTimeSignature(timeSignature: string): this;
+        addKeySignature(key: string): this;
         setContext(context: any): this;
         draw(): void;
     }
@@ -28,6 +29,31 @@ declare module 'vexflow' {
     export class Voice {
         constructor(options: { num_beats: number; beat_value: number });
         addTickables(notes: any[]): this;
+        draw(context: any, stave: Stave): void;
+    }
+    
+    export class Beam {
+        static generateBeams(notes: StaveNote[]): Beam[];
+        setContext(context: any): this;
+        draw(): void;
+    }
+    
+    export class Tuplet {
+        constructor(notes: StaveNote[], options?: { num_notes?: number; notes_occupied?: number });
+        setContext(context: any): this;
+        draw(): void;
+    }
+    
+    export class StaveConnector {
+        static type: {
+            BRACE: number;
+            BRACKET: number;
+            SINGLE: number;
+        };
+        constructor(top_stave: Stave, bottom_stave: Stave);
+        setType(type: number): this;
+        setContext(context: any): this;
+        draw(): void;
         draw(context: any, stave: Stave): void;
     }
     
