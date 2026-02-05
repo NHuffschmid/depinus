ff2243d4cf14473cb12a45f10c5333936f2402ba
Norbert Huffschmid <124078844+NHuffschmid@users.noreply.github.com>
2026-02-05 14:34:04 +0100

    improved packaging (for score support)

--- www/client/vite.config.ts
+++ www/client/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import electron from 'vite-plugin-electron-renderer'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    electron()
+  ],
   base: '/',
   server: {
     open: false
