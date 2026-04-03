985b581e9ba0fd35c71a09eeca756a4e59b3ee43
Norbert Huffschmid <124078844+NHuffschmid@users.noreply.github.com>
2026-04-03 16:38:54 +0200

    fixed git submodule command

--- scripts/build_agent.js
+++ scripts/build_agent.js
@@ -40,7 +40,7 @@ process.stdin.once('data', () => {
 			process.exit(1);
 		}
 		const { user, path } = buildAgents[platform];
-		const sshCmd = `ssh ${user} "bash -l -c 'cd ${path} && git fetch && git checkout ${branch} && git pull && git submodule update && ${buildCmd}'"`;
+		const sshCmd = `ssh ${user} "bash -l -c 'cd ${path} && git fetch && git checkout ${branch} && git pull && git submodule update --init --recursive && ${buildCmd}'"`;
 		execSync(sshCmd, { stdio: 'inherit' });
 	}
 
